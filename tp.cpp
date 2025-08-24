f3a38478e768a9db8c08beb73a4361e057932b53
paras <doyeparas196@gmail.com>
2025-08-24 14:00:58 +0530

    stack

---
+++ tp.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+bool prime(50){
+  for(int i=2;i<=n/2;i++){
+    if(i%2==0){
+      return false;
+    }
+    return true;
+  }
+};
+
+int main(){
+
+  vector<int> pr;
+  int n=50;
+
+  for(int i=2;i<=n;i++){
+  if(prime){
+    pr.push_back(i);
+  }
+}
+ for(int j=0;j<=pr;j++){
+  cout<<pr[j];
+ }
+   
+   return 0;
+}
